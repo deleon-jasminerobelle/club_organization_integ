cd7f3d21ad1693ccfe9c7060977231169644b5fc
deleon-jasminerobelle <jd8312288@gmail.com>
2025-08-29 20:38:33 +0800

    commit

--- pup-club-organization/resources/views/club.blade.php
+++ pup-club-organization/resources/views/club.blade.php
@@ -44,13 +44,13 @@
         <span class="text-lg font-semibold text-maroon">Clubs & Organizations</span>
       </div>
       <div class="hidden md:flex space-x-8">
-        <a href="{{ route('index') }}#home" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">Home</a>
+        <a href="{{ route('index') }}" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">Home</a>
         <a href="{{ route('club') }}" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">Clubs</a>
         <a href="{{ route('events') }}" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">Events</a> 
         <a href="{{ route('news.list') }}" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">News & Media</a>
         <a href="{{ route('gallery') }}" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">Gallery</a>
         <a href="{{ route('about') }}" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">About</a>
-        <a href="{{ route('index') }}#contact" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">Contact</a>
+        <a href="{{ route('index') }}" class="text-maroon hover:text-red-800 transition-all duration-300 font-medium hover:scale-110">Contact</a>
    
         <!-- Logout Button -->
                 <form method="POST" action="{{ route('logout') }}">
